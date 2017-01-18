064a4af1e0896fa20d184d8111983be09ee2fc3b
Christopher Ras <chris@wcw-stud-145-109-1-148.wireless.uva.nl>
2017-01-18 13:34:45 +0100

    Games are stored correctly in Firebase (For now)

--- GameOn/Game.swift
+++ GameOn/Game.swift
@@ -1,5 +1,5 @@
 //
-//  Classes.swift
+//  Game.swift
 //  GameOn
 //
 //  Created by Christopher Ras on 13/01/2017.
@@ -17,6 +17,7 @@ class Game {
     var summary: String?
     var username: String?
     
+    /// Saves the game in Firebase
     func saveToFirebase(myFirebase: FIRDatabaseReference) {
         let currentUser = FIRAuth.auth()!.currentUser!.uid
         let dict = ["title": self.title!, "releaseDate": self.releaseDate!, "coverUrl": self.coverUrl!, "summary": self.summary!]
@@ -25,13 +26,32 @@ class Game {
             self.username = value?["username"] as? String
             
             myFirebase.child("users").child(currentUser).child("Games").child(self.title!.replacingOccurrences(of: ".", with: " ")).setValue(dict)
-            myFirebase.child("Games").child(self.title!.replacingOccurrences(of: ".", with: " ")).child(self.username!).setValue(currentUser)
-            
+            self.addToDict(ref: myFirebase, key: self.username!, value: currentUser, gameTitle: self.title!.replacingOccurrences(of: ".", with: " "))
             
         }) { (error) in
             print(error.localizedDescription)
         }
         
+    }
+    
+    /// Function that can add a new key-value pair to an existing Firebase child
+    private func addToDict(ref: FIRDatabaseReference, key: String, value: String, gameTitle: String) {
+        ref.observeSingleEvent(of: .value, with: { (snapshot) in
+            if snapshot.hasChild("Games") {
+                let gameList = snapshot.childSnapshot(forPath: "Games")
+                    if gameList.hasChild(gameTitle) {
+                        let gameKey = gameList.childSnapshot(forPath: gameTitle)
+                        let dict = gameKey.value as! NSDictionary
+                        let newDict = dict as! NSMutableDictionary
+                        newDict[key] = value
+                        ref.child("Games").child(gameTitle).setValue(newDict)
+                    } else {
+                        ref.child("Games").child(gameTitle).setValue([key: value])
+                    }
+            } else {
+                ref.child("Games").child(gameTitle).setValue([key: value])
+            }
+        })
     }
     
 }
