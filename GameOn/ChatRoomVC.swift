89127ed5b327ccb7e599551e219b1ebafa472e87
Christopher Ras <chris@wcw-stud-145-109-44-98.wireless.uva.nl>
2017-01-19 14:09:38 +0100

    Images and Movies can now be sent in chat

--- GameOn/ChatRoomVC.swift
+++ GameOn/ChatRoomVC.swift
@@ -11,15 +11,17 @@ import JSQMessagesViewController
 import MobileCoreServices
 import AVKit
 
-class ChatRoomVC: JSQMessagesViewController {
+class ChatRoomVC: JSQMessagesViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     private var messages = [JSQMessage]()
+    let picker = UIImagePickerController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tabBarController?.tabBar.isHidden = true
         self.senderId = "1"
         self.senderDisplayName = "Chris"
+        picker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -54,6 +56,54 @@ class ChatRoomVC: JSQMessagesViewController {
         finishSendingMessage()
     }
     
+    override func didPressAccessoryButton(_ sender: UIButton!) {
+        let alert = UIAlertController(title: "Media Messages", message: "Please Select A Media", preferredStyle: .actionSheet)
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        let photos = UIAlertAction(title: "Photos", style: .default, handler: {(alert: UIAlertAction) in
+            self.chooseMedia(type: kUTTypeImage)
+        })
+        
+        let videos = UIAlertAction(title: "Videos", style: .default, handler: {(alert: UIAlertAction) in
+            self.chooseMedia(type: kUTTypeMovie)
+        })
+        alert.addAction(photos)
+        alert.addAction(videos)
+        alert.addAction(cancel)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, didTapMessageBubbleAt indexPath: IndexPath!) {
+        let msg = messages[indexPath.item]
+        
+        if msg.isMediaMessage {
+            if let mediaItem = msg.media as? JSQVideoMediaItem {
+                let player = AVPlayer(url: mediaItem.fileURL)
+                let playerController = AVPlayerViewController()
+                playerController.player = player
+                self.present(playerController, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    private func chooseMedia(type: CFString) {
+        picker.mediaTypes = [type as String]
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let pick = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            let img = JSQPhotoMediaItem(image: pick)
+            self.messages.append(JSQMessage(senderId: senderId, displayName: senderDisplayName, media: img))
+            
+        } else if let vidUrl = info[UIImagePickerControllerMediaURL] as? URL {
+            let video = JSQVideoMediaItem(fileURL: vidUrl, isReadyToPlay: true)
+            self.messages.append(JSQMessage(senderId: senderId, displayName: senderDisplayName, media: video))
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+        collectionView.reloadData()
+    }
     
     
 
