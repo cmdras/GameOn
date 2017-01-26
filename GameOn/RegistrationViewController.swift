01acec8eb976a48e5d4590b60eeab626f5561062
Christopher Ras <chris@wcw-stud-145-109-55-249.wireless.uva.nl>
2017-01-26 15:09:33 +0100

    Users can now add a profile image to their account

--- GameOn/RegistrationViewController.swift
+++ GameOn/RegistrationViewController.swift
@@ -5,23 +5,33 @@
 //  Created by Christopher Ras on 17/01/2017.
 //  Copyright © 2017 Chris Ras. All rights reserved.
 //
+//  Gesture recognizer code adapted from: http://stackoverflow.com/questions/29202882/how-to-you-make-a-uiimageview-on-the-storyboard-clickable-swift
 
 import UIKit
 import Firebase
 
-class RegistrationViewController: UIViewController {
+typealias imageUploaded = () -> ()
+
+class RegistrationViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    @IBOutlet weak var profileImage: UIImageView!
     @IBOutlet weak var userNameInput: UITextField!
     @IBOutlet weak var emailInput: UITextField!
     @IBOutlet weak var passwordInput: UITextField!
     
-    var userID: String?
     var ref: FIRDatabaseReference!
+    let stockURL = "https://firebasestorage.googleapis.com/v0/b/game-on-11c52.appspot.com/o/Profile_Pictures%2Fuser_stock.png?alt=media&token=d5f91d45-bf31-44d7-af92-ac734a1bdfff"
+    var selectedProfileImage: String?
+    
+    let imagePicker = UIImagePickerController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.passwordInput.isSecureTextEntry = true
         ref = FIRDatabase.database().reference()
+        imagePicker.delegate = self
+        profileImage.image = #imageLiteral(resourceName: "user_stock")
+        makeImageTouchable(imageView: profileImage)
 
         // Do any additional setup after loading the view.
     }
@@ -51,10 +61,22 @@ class RegistrationViewController: UIViewController {
                 } else {
                     FIRAuth.auth()?.createUser(withEmail: self.emailInput.text!, password: self.passwordInput.text!, completion: { (user, error) in
                         if error == nil {
-                            self.userID = user!.uid
-                            self.ref.child("users").child(user!.uid).setValue(["Email": user!.email])
                             self.ref.child("users").child(user!.uid).setValue(["username": self.userNameInput.text!])
-                            self.ref.child("usernames").child(self.userNameInput.text!).setValue(["ID": user!.uid])
+                            
+                            let changeRequest = user?.profileChangeRequest()
+                            changeRequest?.displayName = self.userNameInput.text
+                            
+                            if self.profileImage.image == #imageLiteral(resourceName: "user_stock") {
+                                changeRequest?.photoURL = URL(string: self.stockURL)
+                                self.commitChangesToFirebase(user: user, changeRequest: changeRequest)
+                            } else {
+                                self.uploadProfileImageToFirebase(user: user!, completion: {
+                                    changeRequest?.photoURL = URL(string: self.selectedProfileImage!)
+                                    self.commitChangesToFirebase(user: user, changeRequest: changeRequest)
+                                })
+                                
+                            }
+                            
                             self.performSegue(withIdentifier: "loginSegue", sender: nil)
                         } else {
                             let alertController = UIAlertController(title: "Oops", message: error?.localizedDescription, preferredStyle: .alert)
@@ -70,8 +92,68 @@ class RegistrationViewController: UIViewController {
         
     }
     
+    func imageTapped(gesture: UIGestureRecognizer) {
+        if let imageView = gesture.view as? UIImageView {
+            print("image tapped")
+            imagePicker.allowsEditing = false
+            imagePicker.sourceType = .photoLibrary
+            present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    func uploadProfileImageToFirebase(user: FIRUser, completion: @escaping imageUploaded) {
+        let storageRef = FIRStorage.storage().reference(forURL: "gs://game-on-11c52.appspot.com")
+        //let filePath = "\(user.uid)"
+        
+        
+        var data = Data()
+        data = UIImageJPEGRepresentation(profileImage.image!, 0.8)!
+        
+        let metaData = FIRStorageMetadata()
+        metaData.contentType = "image/jpg"
+        
+        storageRef.child("Profile_Pictures").child(user.uid).put(data, metadata: metaData) {(metaData, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            } else {
+                self.selectedProfileImage = metaData!.downloadURL()!.absoluteString
+                completion()
+            }
+        
+        }
+        
+    }
+    
+    func makeImageTouchable(imageView: UIImageView) {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(imageTapped(gesture:)))
+        
+        imageView.addGestureRecognizer(tapGesture)
+        imageView.isUserInteractionEnabled = true
+    }
+    
     @IBAction func cancelTouched(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            self.profileImage.contentMode = .scaleAspectFit
+            self.profileImage.image = pickedImage
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func commitChangesToFirebase(user: FIRUser?,changeRequest: FIRUserProfileChangeRequest?) {
+        changeRequest?.commitChanges { error in
+            if let error = error {
+                print(error)
+            } else {
+                self.ref.child("usernames").child(user!.displayName!).setValue(["ID": user!.uid, "ProfileImage": "\(user!.photoURL!)"])
+                print("Success")
+            }
+        }
     }
     
     
