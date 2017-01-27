354f50639ad4bfbd26ad8d08cb1a06042cfcbdab
Christopher Ras <chris@MacBook-Pro-2.local>
2017-01-27 01:03:49 +0100

    Images are now sent properly to firebase and retrieved. The retrieval does not happen immediately however (bug)

--- GameOn/ChatRoomVC.swift
+++ GameOn/ChatRoomVC.swift
@@ -13,10 +13,13 @@ import JSQMessagesViewController
 import MobileCoreServices
 import AVKit
 import SDWebImage
+import Photos
 
-class ChatRoomVC: JSQMessagesViewController, MessageReceivedDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ChatRoomVC: JSQMessagesViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     private var messages = [JSQMessage]()
+    private var photoMessageMap = [String: JSQPhotoMediaItem]()
+    private var updatedMessageRefHandle: FIRDatabaseHandle?
     let ref = FIRDatabase.database().reference().child("users")
     var roomRef: FIRDatabaseReference?
     var messagesRef: FIRDatabaseReference?
@@ -32,7 +35,6 @@ class ChatRoomVC: JSQMessagesViewController, MessageReceivedDelegate, UIImagePic
         
         self.title = player
         
-        MessageHandler.Instance.delegate = self
         self.tabBarController?.tabBar.isHidden = true
         self.navigationItem.hidesBackButton = true
         let newBackButton = UIBarButtonItem(title: "Back", style: UIBarButtonItemStyle.plain, target: self, action: #selector(ChatRoomVC.back(sender:)))
@@ -49,9 +51,8 @@ class ChatRoomVC: JSQMessagesViewController, MessageReceivedDelegate, UIImagePic
         roomRef = FIRDatabase.database().reference().child("Chatrooms").child(roomID!)
         messagesRef = roomRef!.child("Messages")
         mediaRef = roomRef!.child("Media Messages")
-        MessageHandler.Instance.observeMessages(messagesRef: messagesRef!)
-        MessageHandler.Instance.observeMediaMessages(messagesRef: mediaRef!)
         
+        observeMessages(messagesRef: messagesRef!)
         self.scrollToBottom(animated: true)
     }
     
@@ -165,40 +166,97 @@ class ChatRoomVC: JSQMessagesViewController, MessageReceivedDelegate, UIImagePic
         self.scrollToBottom(animated: true)
     }
     
-    func mediaReceived(senderID: String, senderName: String, url: String) {
-        if let mediaURL = URL(string: url) {
-            do {
-                let data = try Data(contentsOf: mediaURL)
-                if let _ = UIImage(data: data) {
-                    let _ = SDWebImageDownloader.shared().downloadImage(with: mediaURL, options: [], progress: nil, completed: { (image, data, error, finished) in
-                        DispatchQueue.main.async {
-                            let photo = JSQPhotoMediaItem(image: image)
-                            if senderID == self.senderId {
-                                photo?.appliesMediaViewMaskAsOutgoing = true
-                            } else {
-                                photo?.appliesMediaViewMaskAsOutgoing = false
-                            }
-                            
-                            self.messages.append(JSQMessage(senderId: senderID, displayName: senderName, media: photo))
-                            self.collectionView.reloadData()
-                        }
-                    })
-                } else {
-                    let video = JSQVideoMediaItem(fileURL: mediaURL, isReadyToPlay: true)
-                    if senderID == self.senderId {
-                        video?.appliesMediaViewMaskAsOutgoing = true
-                    } else {
-                        video?.appliesMediaViewMaskAsOutgoing = false
-                    }
-                    
-                    messages.append(JSQMessage(senderId: senderID, displayName: senderName, media: video))
-                    self.collectionView.reloadData()
-                }
-            } catch {
-                // errors
+    private func addPhotoMessage(withId id: String, key: String, mediaItem: JSQPhotoMediaItem) {
+        if let message = JSQMessage(senderId: id, displayName: "", media: mediaItem) {
+            messages.append(message)
+            
+            if (mediaItem.image == nil) {
+                photoMessageMap[key] = mediaItem
             }
+            
+            collectionView.reloadData()
+            self.scrollToBottom(animated: true)
         }
     }
+    
+    
+    private func fetchImageDataAtURL(_ photoURL: String, forMediaItem mediaItem: JSQPhotoMediaItem, clearsPhotoMessageMapOnSuccessForKey key: String?) {
+        // 1
+        let storageRef = FIRStorage.storage().reference(forURL: photoURL)
+        
+        // 2
+        storageRef.data(withMaxSize: INT64_MAX){ (data, error) in
+            if let error = error {
+                print("Error downloading image data: \(error)")
+                return
+            }
+            
+            // 3
+            storageRef.metadata(completion: { (metadata, metadataErr) in
+                if let error = metadataErr {
+                    print("Error downloading metadata: \(error)")
+                    return
+                }
+                
+                // 4
+                if (metadata?.contentType == "image/gif") {
+                    mediaItem.image = UIImage.sd_animatedGIF(with: data!)
+                } else {
+                    mediaItem.image = UIImage.init(data: data!)
+                }
+                self.collectionView.reloadData()
+                
+                // 5
+                guard key != nil else {
+                    return
+                }
+                self.photoMessageMap.removeValue(forKey: key!)
+            })
+        }
+    }
+    
+
+    
+    func observeMessages(messagesRef: FIRDatabaseReference) {
+        messagesRef.observe(FIRDataEventType.childAdded) { (snapshot: FIRDataSnapshot) in
+            if let data = snapshot.value as? NSDictionary {
+                if let senderID = data[Constants.SENDER_ID] as? String {
+                    if let senderName = data[Constants.SENDER_NAME] as? String {
+                        if let text = data[Constants.TEXT] as? String {
+                            self.messageReceived(senderID: senderID, senderName: senderName, text: text)
+
+                        } else if let photoURL = data[Constants.URL] as? String {
+                            if let mediaItem = JSQPhotoMediaItem(maskAsOutgoing: senderID == self.senderId) {
+                                self.addPhotoMessage(withId: senderID, key: snapshot.key, mediaItem: mediaItem)
+                                if photoURL.hasPrefix("https://") {
+                                    self.fetchImageDataAtURL(photoURL, forMediaItem: mediaItem, clearsPhotoMessageMapOnSuccessForKey: nil)
+                                    self.collectionView.reloadData()
+                                }
+                            }
+                        }
+                        
+                        
+                    }
+                    
+                }
+                
+            }
+            
+        }
+        
+        updatedMessageRefHandle = messagesRef.observe(.childChanged, with: { (snapshot) in
+            let key = snapshot.key
+            let messageData = snapshot.value as! Dictionary<String, String>
+            
+            if let photoURL = messageData[Constants.URL] as String! {
+                // The photo has been updated.
+                if let mediaItem = self.photoMessageMap[key] {
+                    self.fetchImageDataAtURL(photoURL, forMediaItem: mediaItem, clearsPhotoMessageMapOnSuccessForKey: key)
+                }
+            }
+        })
+    }
+    
     
     func back(sender: UIBarButtonItem) {
         self.tabBarController?.tabBar.isHidden = false
--- GameOn/MessageHandler.swift
+++ GameOn/MessageHandler.swift
@@ -10,16 +10,9 @@
 import Foundation
 import Firebase
 
-protocol MessageReceivedDelegate: class {
-    func messageReceived(senderID: String, senderName: String, text: String)
-    func mediaReceived(senderID: String, senderName: String, url: String)
-}
-
 class MessageHandler {
     private static let _instance = MessageHandler()
     private init() {}
-    
-    weak var delegate: MessageReceivedDelegate?
     
     static var Instance: MessageHandler {
         return _instance
@@ -30,10 +23,10 @@ class MessageHandler {
         messagesRef.child("Messages").childByAutoId().setValue(data)
     }
     
-    func sendMediaMesage(senderID: String, senderName: String, url: String, mediaMessagesRef: FIRDatabaseReference) {
+    func sendMediaMesage(senderID: String, senderName: String, url: String, messagesRef: FIRDatabaseReference) {
         let data = [Constants.SENDER_ID: senderID, Constants.SENDER_NAME: senderName, Constants.URL: url]
         
-        mediaMessagesRef.child("Media Messages").childByAutoId().setValue(data)
+        messagesRef.child("Messages").childByAutoId().setValue(data)
     }
     
     func storeMedia(image: Data?, video: URL?, senderID: String, senderName: String, mediaMessagesRef: FIRDatabaseReference) {
@@ -46,7 +39,7 @@ class MessageHandler {
                 if err != nil {
                     // error
                 } else {
-                    self.sendMediaMesage(senderID: senderID, senderName: senderName, url: String(describing: metadata!.downloadURL()!), mediaMessagesRef: mediaMessagesRef)
+                    self.sendMediaMesage(senderID: senderID, senderName: senderName, url: String(describing: metadata!.downloadURL()!), messagesRef: mediaMessagesRef)
                 }
             
             }
@@ -57,41 +50,12 @@ class MessageHandler {
                 if err != nil {
                     // error
                 } else {
-                    self.sendMediaMesage(senderID: senderID, senderName: senderName, url: String(describing: metadata!.downloadURL()!), mediaMessagesRef: mediaMessagesRef)
+                    self.sendMediaMesage(senderID: senderID, senderName: senderName, url: String(describing: metadata!.downloadURL()!), messagesRef: mediaMessagesRef)
                 }
             
             }
         }
         
-    }
-    
-    func observeMessages(messagesRef: FIRDatabaseReference) {
-        messagesRef.observe(FIRDataEventType.childAdded) { (snapshot: FIRDataSnapshot) in
-            if let data = snapshot.value as? NSDictionary {
-                if let senderID = data[Constants.SENDER_ID] as? String {
-                    if let senderName = data[Constants.SENDER_NAME] as? String {
-                        if let text = data[Constants.TEXT] as? String {
-                            self.delegate?.messageReceived(senderID: senderID, senderName: senderName, text: text)
-                        }
-                    }
-                    
-                }
-            }
-        }
-    }
-    
-    func observeMediaMessages(messagesRef: FIRDatabaseReference) {
-        messagesRef.observe(FIRDataEventType.childAdded) { (snapshot: FIRDataSnapshot) in
-            if let data = snapshot.value as? NSDictionary {
-                if let id = data[Constants.SENDER_ID] as? String {
-                    if let name = data[Constants.SENDER_NAME] as? String {
-                        if let fileURL = data[Constants.URL] as? String {
-                            self.delegate?.mediaReceived(senderID: id, senderName: name, url: fileURL)
-                        }
-                    }
-                }
-            }
-        }
     }
     
 }
